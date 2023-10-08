cb26a37f4e33f3804f91022261b2fb2aba635a08
Shammika Dilshan <31384200+Fordmustangshammika@users.noreply.github.com>
2023-10-09 00:41:05 +0400

    Add files via upload

---
+++ Project/Phone Shop/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //step : 1
+        Phone samsungNote8;
+        new Phone();
+
+        //step : 2
+        Phone samsungNote8 = new Phone();
+    }
+}
---
+++ Project/Phone Shop/Phone.java
@@ -0,0 +1,13 @@
+public class Phone {
+
+    String name;
+    String color;
+    int battery;
+    double price;
+
+    public void call(){}
+    public void sendText(){}
+    public void findPlaces(){}
+    public void takePhotos(){}
+
+}
